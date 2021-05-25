b39ec83c56657335a9baed8ae83cc5b09d7770ec
Enrique720 <enrique.sobrados@utec.edu.pe>
2021-05-24 23:27:16 -0500

    knn

--- KNN.py
+++ KNN.py
@@ -1,9 +1,16 @@
 from sklearn.neighbors import NearestNeighbors
+from sklearn.neighbors import KDTree
 import numpy as np
 from main import read_data
 
 
 
 X = read_data()
-nbrs = NearestNeighbors(n_neighbors=2, algorithm='ball_tree').fit(X)
-distances, indices = nbrs.kneighbors(X)
+#nbrs = NearestNeighbors(n_neighbors=2, algorithm='ball_tree').fit(X)
+#distances, indices = nbrs.kneighbors(X)
+
+
+kdt = KDTree(X, leaf_size=30, metric='euclidean')
+response = kdt.query(X, k=2, return_distance=False)
+
+print(response)
