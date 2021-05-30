9b14ddbce86e4b19b051ef9789bb0495003f1143
cesar <cesar.madera@utec.edu.pe>
2021-05-30 13:44:36 -0500

    some refactoring

--- error_functions.py
+++ error_functions.py
@@ -3,6 +3,15 @@ from sklearn.utils import resample
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import KFold
 
+def distribute_data(train,test):
+    train_data =[]
+    test_data = []
+    for item in train:
+        train_data.append(item["data"])
+    for item in test:
+        test_data.append(item["data"])
+    return train_data,test_data
+
 
 def KFoldValidation(K,method): # K-folds N<K
     data = read_data()
@@ -10,25 +19,15 @@ def KFoldValidation(K,method): # K-folds N<K
     kf = KFold(n_splits=K)
     accuracy = 0
     for train_index, test_index in kf.split(data_train):
-        train_data =[]
-        test_data = []
-        train, test = data_train[train_index], data_train[test_index]
-        for item in train:
-            train_data.append(item["data"])
-        for item in test:
-            test_data.append(item["data"])
+        train, test = data[train_index], data[test_index]
+        train_data,test_data = distribute_data(train,test)
         temp = method(train,test,train_data,test_data)
         print(temp)
         accuracy+=temp
     accuracy /= K
     print("Final estimator accuracy: ", accuracy)
     print("Final estimator error: ", 1-accuracy)
-    train_data =[]
-    test_data = []
-    for item in data_train:
-        train_data.append(item["data"])
-    for item in data_test:
-        test_data.append(item["data"])
+    train_data,test_data = distribute_data(data_train,data_test)
     accuracy = method(data_train,data_test,train_data,test_data,"KFold",True)
     print("Model accuracy: ", accuracy)
     print("Model error: ", 1-accuracy)
@@ -42,24 +41,14 @@ def bootstrap(K,method): # K-folds N<K
     for rep in range(K):
         boot_sample = resample(data_train, n_samples=len(data_train))
         test_set = [x for x in data if x not in boot_sample]
-        train_data =[]
-        test_data = []
-        for item in boot_sample:
-            train_data.append(item["data"])
-        for item in test_set:
-            test_data.append(item["data"])
+        train_data,test_data = distribute_data(boot_sample,test_set)
         temp = method(boot_sample,test_set,train_data,test_data)
         print(temp)
         accuracy+=temp
     accuracy /= K
     print("Final estimator accuracy: ", accuracy)
     print("Final estimator error: ", 1-accuracy)
-    train_data =[]
-    test_data = []
-    for item in data_train:
-        train_data.append(item["data"])
-    for item in data_test:
-        test_data.append(item["data"])
+    train_data,test_data = distribute_data(data_train,data_test)
     accuracy = method(data_train,data_test,train_data,test_data,"Bootstrap",True)
     print("Model accuracy: ", accuracy)
     print("Model error: ", 1-accuracy)    
