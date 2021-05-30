58f57dd20a1f797d64835fe4c8f319ccdf7a5f22
Stephano Wurttele <stephano.wurttele@utec.edu.pe>
2021-05-30 17:44:19 -0500

    varianza'

--- error_functions.py
+++ error_functions.py
@@ -3,6 +3,7 @@ from sklearn.utils import resample
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import KFold
 import matplotlib.pyplot as plt
+from numpy import mean, var
 def distribute_data(train,test):
     train_data =[]
     test_data = []
@@ -16,19 +17,21 @@ def set_options(method_name):
     if(method_name == "knn"):
         return [1, 2, 4, 6, 8, 10, 12, 14]
     elif(method_name == "DT"):
-        return [2,4,8,16,32]
+        return [2, 4, 8, 10, 14, 16, 24, 32, 36]
     elif(method_name == "SVM"):
-        return [1e-2, 1e-3, 1e-4, 1e-5]
+        return [1e1, 1e-1, 1, 1e-2, 1e-3, 1e-4, 1e-5]
 
 
 def fold(data,kf, data_train, K, option, method):
-    accuracy = 0
+    accuracies = []
     for train_index, test_index in kf.split(data_train):
         train, test = data[train_index], data[test_index]
         train_data,test_data = distribute_data(train,test)
         temp = method(train,test,train_data,test_data, option)
-        accuracy+=temp
-    return accuracy/K
+        accuracies.append(temp)
+    promedio = mean(accuracies)
+    varianza = var(accuracies)
+    return [promedio, varianza]
 
 def plot(options,accuracies,method_name,type):
     plt.axis([0,options[len(options)-1]+1,0,1])
@@ -38,21 +41,24 @@ def plot(options,accuracies,method_name,type):
     plt.clf()
 
 def KFoldValidation(K,method): # K-folds N<K
-    
     data = read_data()
     method_name = method.__name__
     data_train, data_test = train_test_split(data, test_size=0.3)
     kf = KFold(n_splits=K)
     options = set_options(method_name)
     accuracies = []
+    variances = []
     errors = []
     for option in options:
-        temp_accuracy = fold(data, kf, data_train, K, option, method)
+        results = fold(data, kf, data_train, K, option, method)
         print(method_name, "KFold", " - option ", option)
-        print("Final estimator accuracy", temp_accuracy)
-        print("Final estimator error", 1-temp_accuracy)
-        accuracies.append(temp_accuracy)
-        errors.append(1-temp_accuracy)
+        print("Final estimator accuracy", results[0])
+        print("Final estimator error", 1-results[0])
+        accuracies.append(results[0])
+        variances.append(results[1])
+        errors.append(1-results[0])
+    if(method_name == "SVM"):
+        options = [1, 0, -1,- 2, -3, -4, -5]
     plot(options,accuracies,method_name,'Kfold')
     '''train_data,test_data = distribute_data(data_train,data_test)
     accuracy = method(data_train,data_test,train_data,test_data,"KFold",True)
@@ -61,15 +67,16 @@ def KFoldValidation(K,method): # K-folds N<K
     return [accuracies, errors]
     
 def sampling(data, data_train, K, option, method):
-    accuracy = 0
+    accuracies = []
     for rep in range(K):
         boot_sample = resample(data_train, n_samples=len(data_train))
         test_set = [x for x in data if x not in boot_sample]
         train_data,test_data = distribute_data(boot_sample,test_set)
         temp = method(boot_sample,test_set,train_data,test_data, option)
-        accuracy+=temp
-    return accuracy / K
-    
+        accuracies.append(temp)
+    promedio = mean(accuracies)
+    varianza = var(accuracies)
+    return [promedio, varianza]
 
 def bootstrap(K,method): # K-folds N<K
     data = read_data()
@@ -77,14 +84,19 @@ def bootstrap(K,method): # K-folds N<K
     data_train, data_test = train_test_split(data, test_size=0.3)
     options = set_options(method_name)
     accuracies = []
+    variances = []
     errors = []
     for option in options:
-        temp_accuracy = sampling(data, data_train, K, option, method)
+        results = sampling(data, data_train, K, option, method)
         print(method_name, "bootstrap" + " - option ", option)
-        print("Final estimator accuracy", temp_accuracy)
-        print("Final estimator error", 1-temp_accuracy)
-        accuracies.append(temp_accuracy)
-        errors.append(1-temp_accuracy)
+        print("Final estimator accuracy", results[0])
+        print("Final estimator variance", results[1])
+        print("Final estimator error", 1-results[0])
+        accuracies.append(results[0])
+        variances.append(results[1])
+        errors.append(1-results[0])
+    if(method_name == "SVM"):
+        options = [1, 0, -1,- 2, -3, -4, -5]
     plot(options,accuracies,method_name,'bootstrap')
 
     '''train_data,test_data = distribute_data(data_train,data_test)
